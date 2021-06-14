74ee1ae29af5b1dac07d92679dfc5b430aab9ec4
Mathieu Baudet <mathieubaudet@fb.com>
2021-06-14 08:25:32 -0700

    [librabftv2] add missing serialization traits to DataSync*

--- librabft-v2/src/unit_tests/data_sync_tests.rs
+++ librabft-v2/src/unit_tests/data_sync_tests.rs
@@ -1,2 +1,47 @@
 // Copyright (c) Calibra Research
 // SPDX-License-Identifier: Apache-2.0
+
+use super::*;
+use crate::node::NodeConfig;
+use bft_lib::simulated_context::SimulatedContext;
+use serde_json;
+use std::collections::BTreeSet;
+
+#[test]
+fn test_serde_notification() {
+    let data = DataSyncNotification::<SimulatedContext<NodeConfig>> {
+        current_epoch: EpochId(0),
+        highest_commit_certificate: None,
+        highest_quorum_certificate: None,
+        timeouts: Vec::new(),
+        current_vote: None,
+        proposed_block: None,
+    };
+    let message = serde_json::to_string(&data).unwrap();
+    let data2: DataSyncNotification<SimulatedContext<NodeConfig>> =
+        serde_json::from_str(&message).unwrap();
+    assert_eq!(data2, data);
+}
+
+#[test]
+fn test_serde_request() {
+    let data = DataSyncRequest {
+        current_epoch: EpochId(0),
+        known_quorum_certificates: BTreeSet::default(),
+    };
+    let message = serde_json::to_string(&data).unwrap();
+    let data2: DataSyncRequest = serde_json::from_str(&message).unwrap();
+    assert_eq!(data2, data);
+}
+
+#[test]
+fn test_serde_response() {
+    let data = DataSyncResponse::<SimulatedContext<NodeConfig>> {
+        current_epoch: EpochId(0),
+        records: Vec::new(),
+    };
+    let message = serde_json::to_string(&data).unwrap();
+    let data2: DataSyncResponse<SimulatedContext<NodeConfig>> =
+        serde_json::from_str(&message).unwrap();
+    assert_eq!(data2, data);
+}
